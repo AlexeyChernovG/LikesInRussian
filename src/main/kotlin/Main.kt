d0c6b5bd2350d3d8df47c4a4c5288a3251e87757
AlexeyChernovG <ikrevetko18@gmail.com>
2022-07-29 19:11:49 +0600

    Added exceptions concerning number 11

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
 fun main() {
 
-    var likes = 61
+    val likes = 11
 
     val lastDigit = likes % 10
+    val lastDigits = likes % 100
 
     val inRussian:String
 
-    if (lastDigit == 1) inRussian = " человеку" else inRussian = " людям"
+    if (lastDigits == 11) inRussian = " людям" else if (lastDigit == 1) inRussian = " человеку" else inRussian = " людям"
 
     println("Понравилось " + likes + inRussian)
 }
